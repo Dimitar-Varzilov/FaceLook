f2ffba085547ef7c3dfd010bd46cacd10757f6be
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-01-28 00:03:09 +0200

    Add email server options model

---
+++ FaceLook/Services/MailServerOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace FaceLook.Services;
+
+public record MailServerOptions
+{
+    public required string MailServerUserName { get; set; }
+    public required string MailServerPassword { get; set; }
+    public required string MailServerSenderName{ get; set; }
+    public required string MailServerSenderEmail{ get; set; }
+}
