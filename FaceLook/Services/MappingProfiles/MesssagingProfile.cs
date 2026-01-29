b9bdddde7ac58dc41a9cab907cb5054fe1e1b9d8
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-01-29 22:11:13 +0200

    Add messaging auto mapper profile

---
+++ FaceLook/Services/MappingProfiles/MesssagingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FaceLook.Data.Entities;
+using FaceLook.ViewModels;
+
+namespace FaceLook.Services.MappingProfiles
+{
+    public class MesssagingProfile : Profile
+    {
+        public MesssagingProfile()
+        {
+            CreateMap<Message, MessageViewModel>();
+        }
+    }
+}
