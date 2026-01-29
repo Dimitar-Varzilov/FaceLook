67262a942d1e53786696839279b5528162a70d00
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-01-29 22:10:14 +0200

    Add base models

---
+++ FaceLook/ViewModels/BaseViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FaceLook.ViewModels
+{
+    public class BaseViewModel
+    {
+        public required Guid Id { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset? ModifiedAt { get; set; }
+    }
+}
