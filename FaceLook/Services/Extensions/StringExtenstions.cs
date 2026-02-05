66c978d82b59bcab77d7dfbaac63395e20742f94
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-02-05 21:15:26 +0200

    Add StringExtenstions

---
+++ FaceLook/Services/Extensions/StringExtenstions.cs
@@ -0,0 +1,17 @@
+﻿namespace FaceLook.Services.Extensions
+{
+    public static class StringExtenstions
+    {
+        public static string ExtractControllerName(string controllerName)
+        {
+            ArgumentNullException.ThrowIfNullOrWhiteSpace(controllerName, nameof(controllerName));
+
+            const string controllerSubstring = "Controller";
+            if (!controllerName.Contains(controllerSubstring))
+            {
+                throw new Exception($"Argument {nameof(controllerName)}:{controllerName} is not valid controller name.");
+            }
+            return controllerName.Replace(controllerSubstring, string.Empty);
+        }
+    }
+}
