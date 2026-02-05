50a17c9dd08e2cbfff5316e1ee5d9c76c996c5ad
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-02-05 21:06:05 +0200

    Add PictureConstants

---
+++ FaceLook/Constants/PictureConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace FaceLook.Constants
+{
+    public static class PictureConstants
+    {
+        public static readonly IReadOnlyCollection<string> AllowedExtensions = [".jpg", ".jpeg", ".png", ".gif"];
+
+        public const string PictureMessageKey = "PictureMessage";
+        public const string PictureNameKey = "PictureName";
+
+    }
+}
