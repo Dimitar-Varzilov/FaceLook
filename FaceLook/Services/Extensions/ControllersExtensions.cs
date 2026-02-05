80a9c1d7444f13673ee68f0cb49fbe0319cab464
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-02-05 21:35:17 +0200

    Add ControllersExtensions

---
+++ FaceLook/Services/Extensions/ControllersExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FaceLook.Services.Extensions
+{
+    public static class ControllersExtensions
+    {
+        public static string ExtractControllerName<T>(this T controller)
+            where T : Controller
+        {
+            Type controllerType = controller.GetType();
+
+            return controllerType.Name.Replace("Controller", string.Empty);
+        }
+
+        public static string GetControllerName<T>()
+            where T : Controller
+        {
+            Type ttype = typeof(T);
+
+            return ttype.Name.Replace("Controller", string.Empty);
+        }
+    }
+}
