8f08c6368bd3acc9e4080a0cab4ee2c9e0d075f2
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-02-11 20:48:27 +0200

    Add file share options

---
+++ FaceLook/Services/FileShareOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace FaceLook.Services
+{
+    public record FileShareOptions
+    {
+        public required string FileShare { get; set; }
+        public required string ConnectionString { get; set; }
+        public required int SasExpiryInHours { get; set; }
+    }
+}
