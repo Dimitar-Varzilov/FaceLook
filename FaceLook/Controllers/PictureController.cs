c6ad446b63e5b91f3af9effdcdb6bdd7280f95a1
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-02-11 20:57:05 +0200

    use file share service methods in PicturesController

--- FaceLook/Controllers/PictureController.cs
+++ FaceLook/Controllers/PictureController.cs
@@ -1,28 +1,50 @@
 ﻿using FaceLook.Constants;
 using FaceLook.Services;
-using FaceLook.Services.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FaceLook.Controllers
 {
-    public class PicturesController(IBlobService blobService) : Controller
+    [Authorize]
+    public class PicturesController(IFileShareService fileShareService) : Controller
     {
-        public async Task<IActionResult> Upload(IFormFile picture)
+        public async Task<ViewResult> Index()
+        {
+            var pictureNames = await fileShareService.GetCurrentUserPictures();
+            return View(pictureNames);
+        }
+
+        public async Task<FileStreamResult> GetPicture([FromQuery] string pictureName)
+        {
+            var picture = await fileShareService.GetPictureByNameAsync(pictureName);
+            return File(picture.Content, picture.ContentType);
+        }
+        public async Task<IActionResult> Upload()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UploadPicture(IFormFile picture)
         {
             if (picture == null || picture.Length == 0)
                 return BadRequest("No file selected.");
 
-            var allowedExtensions = PictureConstants.AllowedExtensions;
             var extension = Path.GetExtension(picture.FileName).ToLowerInvariant();
 
-            if (!allowedExtensions.Contains(extension))
+            if (!PictureConstants.AllowedExtensions.Contains(extension))
                 return BadRequest("Invalid file type.");
 
-            await blobService.UploadBlob(picture);
+            await fileShareService.UploadFile(picture);
 
             TempData[PictureConstants.PictureMessageKey] = "Picture uploaded sucessfully";
 
-            return RedirectToAction(nameof(HomeController.Index), ControllersExtensions.GetControllerName<HomeController>());
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete([FromQuery] string pictureName)
+        {
+            await fileShareService.DeleteFile(pictureName);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
