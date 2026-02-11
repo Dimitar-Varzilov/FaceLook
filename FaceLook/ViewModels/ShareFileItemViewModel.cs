b422b853fe5fb8bebcb3d72657867eb95e323cc5
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-02-11 20:50:14 +0200

    add file share view model

---
+++ FaceLook/ViewModels/ShareFileItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FaceLook.ViewModels
+{
+    public record ShareFileItemViewModel
+    {
+        public required string Name { get; set; }
+        public required string SasUrl { get; set; }
+    }
+}
