5bd54fabd1a151ec65767e37b59298741f024c5c
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-02-11 21:51:20 +0200

    Add Message controller and input model

---
+++ FaceLook/Controllers/MessagesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FaceLook.Controllers
+{
+    public class MessagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SendMessage()
+        {
+
+        }
+    }
+}
---
+++ FaceLook/ViewModels/SendMessageRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FaceLook.ViewModels
+{
+    public record SendMessageRequest
+    {
+        [EmailAddress]
+        [Required]
+        public required string ReceiverEmail { get; set; }
+
+        [MinLength(1)]
+        [Required]
+        public required string Content { get; set; }
+    }
+}
