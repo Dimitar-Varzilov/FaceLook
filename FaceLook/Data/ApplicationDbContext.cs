015032256286ada23c4476d69bea4a9a9a0b7414
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-02-14 16:12:51 +0200

    Register User in DbContext

--- FaceLook/Data/ApplicationDbContext.cs
+++ FaceLook/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FaceLook.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FaceLook.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<User>(options)
     {
     }
 }
