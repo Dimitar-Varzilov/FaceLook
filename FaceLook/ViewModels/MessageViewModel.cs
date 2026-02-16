6d348c3d4fc23ce3621fdb373c215aaba9d8991e
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-02-16 20:15:31 +0200

    Add User and Status - related properties to MessageViewModel

--- FaceLook/ViewModels/MessageViewModel.cs
+++ FaceLook/ViewModels/MessageViewModel.cs
@@ -1,7 +1,22 @@
-﻿namespace FaceLook.ViewModels
+﻿using FaceLook.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace FaceLook.ViewModels
 {
     public class MessageViewModel : BaseViewModel
     {
+        [Required]
+        public required string SenderId { get; set; }
+
+        [MinLength(1)]
+        [Required]
+        public required string ReceiverId { get; set; }
+
+        [MinLength(1)]
+        [Required]
         public required string Content { get; set; }
+
+        [Required]
+        public required MessageStatus MessageStatus { get; set; }
     }
 }
