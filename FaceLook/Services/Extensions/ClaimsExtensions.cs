f86db9acd0d23db1e539a1c85f7e5fa947d4a6ed
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-02-16 20:27:05 +0200

    Add claims extensions for faster extracting userid and email from cookie token

---
+++ FaceLook/Services/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FaceLook.Services.Exceptions;
+using System.Security.Claims;
+
+namespace FaceLook.Services.Extensions
+{
+    public static class ClaimsExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? throw new ValidationException("Cannot extract the UserId from token");
+        }
+
+        public static string GetEmail(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.FindFirst(ClaimTypes.Email)?.Value ?? throw new ValidationException("Cannot extract the Email from token");
+        }
+    }
+}
