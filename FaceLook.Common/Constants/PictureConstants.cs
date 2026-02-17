c1ddece768000af4cc36f7f7a42c5e6ff224ac3f
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-02-17 10:20:05 +0200

    Add picture error constant key

--- FaceLook.Common/Constants/PictureConstants.cs
+++ FaceLook.Common/Constants/PictureConstants.cs
@@ -5,6 +5,7 @@
         public static readonly IReadOnlyCollection<string> AllowedExtensions = [".jpg", ".jpeg", ".png", ".gif"];
 
         public const string PictureMessageKey = "PictureMessage";
+        public const string PictureErrorKey = "PictureError";
         public const string PictureNameKey = "PictureName";
         public const string PictureSasUriKey = "PictureSasUri";
         public const string PictureSasExpiresOnKey = "PictureSasExpiresOn";
