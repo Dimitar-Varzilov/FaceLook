29aaf8a0e684c378184b952e4ab2d6c2aff0d26d
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-03-07 20:07:33 +0200

    Add CacheService

--- FaceLook.Services/Extensions/ServiceCollectionExtension.cs
+++ FaceLook.Services/Extensions/ServiceCollectionExtension.cs
@@ -33,6 +33,7 @@ namespace FaceLook.Services.Extensions
             services.AddScoped<IMessageService, MessageService>();
             services.AddScoped<IPictureService, PictureService>();
             services.AddScoped<IFriendService, FriendService>();
+            services.AddScoped<ICacheService, CacheService>();
         }
 
         private static void RegisterApplicationServices(IServiceCollection services)
