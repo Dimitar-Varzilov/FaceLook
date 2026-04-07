e22778392cfb6e3803ffe390a17d91edb7aaabd4
Dimitar Varzilov <dimitar_varzilov@hotmail.com>
2026-04-07 14:08:59 +0300

    Add Chat entity models and viewmodels

---
+++ FaceLook.Web.ViewModels/ChatViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FaceLook.Web.ViewModels
+{
+    public class ChatViewModel: BaseNamedViewModel
+    {
+        [MinLength(2)]
+        public virtual ICollection<string> ParticipantIds { get; set; } = null!;
+        public virtual ICollection<MessageViewModel> Messages { get; set; } = null!;
+    }
+}
